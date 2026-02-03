f71a155c1c3b48cef26d1f9c494913d5e05c1849
Eli <163587420+Eli-Snyder@users.noreply.github.com>
2026-02-03 11:06:11 -0600

    Moved GameOver handling to GameManager.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     public bool gameOver;
 
     [SerializeField] private float gravityModifier = 1.5f, floatForce = 5;
+    [SerializeField] private GameManager gameManager; // Assign GameManager GameObject in the editor.
     private Rigidbody playerRb;
 
 
@@ -14,8 +15,6 @@ public class PlayerController : MonoBehaviour
         playerRb = GetComponent<Rigidbody>();
         // Apply a small upward force at the start of the game
         playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
-        
-
     }
 
     // Update is called once per frame
@@ -32,7 +31,7 @@ public class PlayerController : MonoBehaviour
     { 
         if (other.gameObject.CompareTag("Obstacle"))
         {
-            gameOver = true; // This needs to change to trigger the GameOver() method in GameManager.cs
+            gameManager.GameOver();
         } 
     }
 
