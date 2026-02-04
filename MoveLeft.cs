3b945003bcf2a0c8bece3444568ca4673a6f058a
Eli <163587420+Eli-Snyder@users.noreply.github.com>
2026-02-04 15:27:45 +0000

    Update MoveLeft.cs
    Uploaded an updated and tested MoveLeft.cs file

--- MoveLeft.cs
+++ MoveLeft.cs
@@ -1,19 +1,28 @@
-﻿using UnityEngine;
+using UnityEngine;
+
 
 public class MoveLeft : MonoBehaviour
 {
     [SerializeField] private float speed;
-    [SerializeField private GameManager gameManager;
+    public GameManager gameManager;
+
     private float leftBound = -10;
 
+
+    // Update is called once per frame
     void FixedUpdate()
     {
         // If game is not over, move to the left
         if (gameManager.isGameActive)
-        { transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World); }
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+        }
 
         // If object goes off screen that is NOT the background, destroy it
         if (transform.position.x < leftBound && !gameObject.CompareTag("Background"))
-        { Destroy(gameObject); }
+        {
+            Destroy(gameObject);
+        }
+
     }
 }
