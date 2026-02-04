3f36667d29918348a19fdb590ad22a329fdf6f97
Eli <163587420+Eli-Snyder@users.noreply.github.com>
2026-02-04 15:28:29 +0000

    Update PlayerController.cs
    Updated and tested PlayerController.cs file

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,11 +1,9 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public bool gameOver;
-
     [SerializeField] private float gravityModifier = 1.5f, floatForce = 5;
-    [SerializeField] private GameManager gameManager; // Assign GameManager GameObject in the editor.
+    [SerializeField] private GameManager gameManager;
     private Rigidbody playerRb;
 
 
@@ -15,18 +13,29 @@ public class PlayerController : MonoBehaviour
         playerRb = GetComponent<Rigidbody>();
         // Apply a small upward force at the start of the game
         playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+        
+
     }
 
+    // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !gameOver)
-        { playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse); }
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKeyDown(KeyCode.Space) && gameManager.isGameActive)
+        {
+            playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
+            Debug.Log("Boing");
+        }
     }
 
     private void OnCollisionEnter(Collision other)
-    { 
+    {
+        // if player collides with the opps, set gameOver to true
         if (other.gameObject.CompareTag("Obstacle"))
-        { gameManager.GameOver(); } 
+        {
+            gameManager.GameOver();
+            Debug.Log("Rip broski");
+        } 
     }
 
 }
